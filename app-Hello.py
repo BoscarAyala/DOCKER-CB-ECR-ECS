69512ce3bdbb6493e4cb53301afaec79939b4a9f
ayyalab <ayyalab@hotmail.com>
2021-09-05 03:26:21 -0400

    adding code

---
+++ app-Hello.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+     
+@app.route("/")
+def index():
+	return "Welcome to the index page."
+
+@app.route("/hi/")
+def who():
+	return "who are you?"
+
+@app.route("/hi/<username>")
+def greet(username):
+	return "Hi there, " + username + " !!"
